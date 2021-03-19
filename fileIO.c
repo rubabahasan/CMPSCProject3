2e886dc2f0742ce095896411c812676e2c8ba858
rubabahasan <rubabahasanb@gmail.com>
2021-03-19 11:11:55 -0400

    polishing for upload

--- fileIO.c
+++ fileIO.c
@@ -133,8 +133,8 @@ struct NextMem* readNextMem(FILE* fptr)
 
     if((read = getline(&line, &len, fptr)) != -1)
     {
-        printf("Retrieved line of length %zu:\n", read);
-        printf("%s::\n", line);
+        // printf("Retrieved line of length %zu:\n", read);
+        // printf("%s::\n", line);
         struct NextMem* lineRead = (struct NextMem*)malloc(sizeof(struct NextMem));
         line[strcspn(line, "\n")] = '\0'; //removes trailing newline characters, if any
 
@@ -170,7 +170,7 @@ int readNumIns(FILE* fptr)
     {
         token = strtok(line, " ");
         token = strtok(NULL, " ");
-        printf("Read %s::\n", token);
+        // printf("Read %s::\n", token);
         return atoi(token);
     }
     
